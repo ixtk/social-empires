350769006517855bbb0916cba1be1ad81b845bb3
ixtk <ikhcamot@gmail.com>
2022-04-19 13:23:28 +0400

    Stop animations after image load

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,11 @@
+const handleLoad = e => {
+  e.target.style.animation = 'none'
+}
+
 const Image = props => {
   return (
     <figure>
-      <img src={props.src} height="200" width="200"/>
+      <img src={props.src} height="200" width="200" onLoad={handleLoad}/>
       <figcaption>{props.name}</figcaption>
     </figure>
   )
